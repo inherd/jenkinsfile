4da1085171d208c3f8c52e2c398b359e13af6940
Phodal Huang <h@phodal.com>
2021-02-25 22:12:04 +0800

    tests: add multiple tests

--- src/lib.rs
+++ src/lib.rs
@@ -205,4 +205,54 @@ mod tests {
         println!("{:?}", jenkinsfile.stages[0].steps);
         assert_eq!(1, jenkinsfile.stages[0].steps.len());
     }
+
+    #[test]
+    pub fn should_parse_multiple_stages() {
+        let code = r#"pipeline {
+    agent none
+    stages {
+        stage('Build') {
+            agent any
+            steps {
+                checkout scm
+                sh 'make'
+                stash includes: '**/target/*.jar', name: 'app'
+            }
+        }
+        stage('Test on Linux') {
+            agent {
+                label 'linux'
+            }
+            steps {
+                unstash 'app'
+                sh 'make check'
+            }
+            post {
+                always {
+                    junit '**/target/*.xml'
+                }
+            }
+        }
+        stage('Test on Windows') {
+            agent {
+                label 'windows'
+            }
+            steps {
+                unstash 'app'
+                bat 'make check'
+            }
+            post {
+                always {
+                    junit '**/target/*.xml'
+                }
+            }
+        }
+    }
+}
+        "#;
+        let jenkinsfile = Jenkinsfile::from_str(code).unwrap();
+        assert_eq!(3, jenkinsfile.stages.len());
+        println!("{:?}", jenkinsfile.stages);
+        assert_eq!("Build", jenkinsfile.stages[0].name);
+    }
 }
